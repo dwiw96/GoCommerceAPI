4892bdfcd96b8064a3c4b66267e6301502affa01
dwiw96 <dwiwahyudi1996@gmail.com>
2024-11-07 15:33:18 +0700

    add: error message and code

--- pkg/utils/responses/error_handler.go
+++ pkg/utils/responses/error_handler.go
@@ -3,19 +3,23 @@ package responses
 import "errors"
 
 var (
-	CodeSuccess       = 200 // 200
-	CodeSuccessCreate = 201 // 201
+	CodeSuccess       = 200 // 200, OK
+	CodeSuccessCreate = 201 // 201, Created
 	CodeSuccessUpdate = 201 // 201
 
-	CodeFailedServer       = 500 // 500
-	CodeFailedUser         = 400 // 400
-	CodeFailedValidation   = 422 // 422
-	CodeFailedUnauthorized = 401 // 401
-	CodeFailedDuplicated   = 409 // 409
+	CodeFailedServer       = 500 // 500, Internal Server Error
+	CodeFailedUser         = 400 // 400, Bad Request
+	CodeFailedValidation   = 422 // 422, Unprocessably Entity
+	CodeFailedUnauthorized = 401 // 401, Unauthorized
+	CodeFailedDuplicated   = 409 // 409, Conflict
 )
 
 var (
-	ErrNoData    = errors.New("no data found")
-	ErrDuplicate = errors.New("duplicate data")
-	ErrViolation = errors.New("input is no valid")
+	ErrNoData          = errors.New("no data found")  // no data found
+	ErrDuplicate       = errors.New("duplicate data") // duplicate data
+	ErrViolation       = errors.New("invalid input")  // invalid input
+	ErrCheckConstraint = errors.New("invalid input")  // invalid input
+	ErrNotNull         = errors.New("input is empty") // input is empty
+	ErrInvalidInput    = errors.New("invalid input")  // invalid input
+	ErrServer          = errors.New("server error")   // server error
 )
