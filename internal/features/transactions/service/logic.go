3efbc1a04f4f2b80db4068e301b1f5e374781f1c
dwiw96 <dwiwahyudi1996@gmail.com>
2024-11-14 22:39:16 +0700

    create: purchase product feature

---
+++ internal/features/transactions/service/logic.go
@@ -0,0 +1,77 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+
+	transactions "github.com/dwiw96/vocagame-technical-test-backend/internal/features/transactions"
+	errs "github.com/dwiw96/vocagame-technical-test-backend/pkg/utils/responses"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+)
+
+type transactionsService struct {
+	ctx  context.Context
+	repo transactions.IRepository
+}
+
+func NewTransactionsService(ctx context.Context, repo transactions.IRepository) transactions.IService {
+	return &transactionsService{
+		ctx:  ctx,
+		repo: repo,
+	}
+}
+
+func handleError(arg error) (code int, err error) {
+	log.Println(arg)
+	if errors.Is(arg, pgx.ErrNoRows) {
+		log.Println("ERRNOROWS")
+		return errs.CodeFailedUser, errs.ErrNoData
+	}
+	var pgErr *pgconn.PgError
+	if errors.As(arg, &pgErr) {
+		log.Println("pgErr:", pgErr)
+		if pgErr.ConstraintName == "ck_transactions_balance" {
+			return errs.CodeFailedUser, fmt.Errorf("balance minimum is 0")
+		}
+		switch pgErr.Code {
+		case "23505": // UNIQUE violation
+			return errs.CodeFailedDuplicated, errs.ErrDuplicate
+		case "23514": // CHECK violation
+			if pgErr.ConstraintName == "ck_wallets_balance" {
+				return errs.CodeFailedUser, fmt.Errorf("balance is insufficient")
+			}
+			if pgErr.ConstraintName == "ck_products_availability" {
+				return errs.CodeFailedUser, fmt.Errorf("product stock is insufficient")
+			}
+			return errs.CodeFailedUser, errs.ErrCheckConstraint
+		case "23502": // NOT NULL violation
+			return errs.CodeFailedUser, errs.ErrNotNull
+		case "23503": // Foreign Key violation
+			return errs.CodeFailedUser, errs.ErrViolation
+		default:
+			err = fmt.Errorf("database error occurred")
+		}
+	}
+
+	return errs.CodeFailedServer, err
+}
+
+func (s *transactionsService) PurchaseProduct(arg transactions.TransactionParams) (res *transactions.TransactionHistory, code int, err error) {
+	if arg.Quantity.Int32 <= int32(0) {
+		return nil, 400, fmt.Errorf("quantity must be more than 0")
+	}
+	code = 200
+	res, err = s.repo.TransactionPurchaseProduct(arg)
+	if err != nil {
+		code, newErr := handleError(err)
+		log.Println("handlerError code:", code)
+		log.Println("handlerError err:", newErr)
+		return res, code, newErr
+	}
+
+	return
+}
