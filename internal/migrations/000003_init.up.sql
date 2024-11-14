838e9d9cce721c6692514305995426870c89cd87
dwiw96 <dwiwahyudi1996@gmail.com>
2024-11-14 22:29:28 +0700

    modify: separated transaction_histories table to migration 4

--- internal/migrations/000003_init.up.sql
+++ internal/migrations/000003_init.up.sql
@@ -29,33 +29,4 @@ CREATE TABLE wallets(
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
-
-CREATE TYPE transaction_type AS ENUM ('purchase', 'deposit', 'withdrawal');
-CREATE TYPE transaction_status AS ENUM ('completed', 'pending', 'failed');
-
-CREATE TABLE transactions_history(
-    id INT GENERATED ALWAYS AS IDENTITY
-        CONSTRAINT pk_transactions_history_id PRIMARY KEY,
-    from_wallet_id INT NOT NULL,
-        CONSTRAINT fk_transactions_history_from_wallet_id FOREIGN KEY (from_wallet_id)
-            REFERENCES wallets(id),
-    to_wallet_id INT NULL,
-        CONSTRAINT fk_transactions_history_to_wallet_id FOREIGN KEY (to_wallet_id)
-            REFERENCES wallets(id),
-    product_id INT NOT NULL,
-        CONSTRAINT fk_transactions_history_product_id FOREIGN KEY (product_id)
-            REFERENCES products(id),
-    amount INT NOT NULL DEFAULT 0
-        CONSTRAINT ck_transactions_history_amount CHECK (amount >= 0),
-    t_type transaction_type NOT NULL,
-    t_status transaction_status NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW()
-);
-
-CREATE INDEX ix_transactions_history_from_wallet_id ON transactions_history(from_wallet_id);
-CREATE INDEX ix_transactions_history_to_wallet_id ON transactions_history(to_wallet_id);
-CREATE INDEX ix_transactions_history_product_id ON transactions_history(product_id);
-CREATE INDEX ix_transactions_history_t_type ON transactions_history(t_type);
-CREATE INDEX ix_transactions_history_t_status ON transactions_history(t_status);
-CREATE INDEX ix_transactions_history_created_at ON transactions_history(created_at);
 COMMIT;
